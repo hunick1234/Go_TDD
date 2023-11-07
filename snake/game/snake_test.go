928b5a0c26abea7704c05955726339026ec44435
邵軒 胡 <hunick1234@gmail.com>
2023-11-07 23:28:22 +0800

    test move

--- snake/game/snake_test.go
+++ snake/game/snake_test.go
@@ -5,11 +5,37 @@ import (
 	"testing"
 )
 
-func TestMoveUp(t *testing.T) {
+func TestMove(t *testing.T) {
 	g := &game.GameObject{0, 0}
 	initialY := g.Y
-	g.MoveUp()
-	if g.Y != initialY-1 {
-		t.Errorf("MoveUp failed, expected %d, got %d", initialY-1, g.Y)
-	}
+	initialX := g.X
+
+	t.Run("MoveDown", func(t *testing.T) {
+		g.MoveDown()
+		if g.Y != initialY-1 {
+			t.Errorf("MoveDown failed, expected %d, got %d", initialY-1, g.Y)
+		}
+	})
+
+	t.Run("MoveUp", func(t *testing.T) {
+		g.MoveUp()
+		if g.Y != initialY {
+			t.Errorf("MoveUp failed, expected %d, got %d", initialY, g.Y)
+		}
+	})
+
+	t.Run("MoveLeft", func(t *testing.T) {
+		g.MoveLeft()
+		if g.X != initialX-1 {
+			t.Errorf("MoveLeft failed, expected %d, got %d", initialX-1, g.X)
+		}
+	})
+
+	t.Run("MoveRight", func(t *testing.T) {
+		g.MoveRight()
+		if g.X != initialX {
+			t.Errorf("MoveRight failed, expected %d, got %d", initialX, g.X)
+		}
+	})
+
 }
